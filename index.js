04d4fff0712101fcdf7445403fae597fe910db4f
ebitgeek <ebitgeek@gmail.com>
2019-05-22 23:12:53 +0800

    添加cookieJar

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const request = require('request-promise')
 const axios = require('axios')
 
 async function main () {
-  let response = await request.get('https://google.com', { proxy: 'socks5://127.0.0.1:1080', followAllRedirects: true, resolveWithFullResponse: true })
+  let jar = request.jar()
+  let response = await request.get('https://google.com', { proxy: 'socks5://127.0.0.1:1080', followAllRedirects: true, resolveWithFullResponse: true, jar: jar })
   console.log(response)
   // let response = await axios.default.get('http://baidu.com')
   // console.log(response)
