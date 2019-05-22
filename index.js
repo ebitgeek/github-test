189b9f22647438fe9e6ae62204434f64569d31b9
ebitgeek <ebitgeek@gmail.com>
2019-05-22 23:46:16 +0800

    添加了一个测试cookie

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const request = require('request-promise')
-const axios = require('axios')
 
-async function main () {
+async function requestGetGoogle({ proxy, followAllRedirects, resolveWithFullResponse, jar}) {
+  let resonpse = await request.get('https://www.google.com', { proxy, followAllRedirects, jar, resolveWithFullResponse, rejectUnauthorized: false })
+  return resonpse;
+}
+
+async function main() {
   let jar = request.jar()
-  let response = await request.get('https://google.com', { proxy: 'socks5://127.0.0.1:1080', followAllRedirects: true, resolveWithFullResponse: true, jar: jar })
+  let proxy = 'http://127.0.0.1:8080'
+  let followAllRedirects = true
+  let resolveWithFullResponse = true
+
+  jar.setCookie(request.cookie('test=test'), 'https://www.google.com')
+  let response = await requestGetGoogle({ proxy, followAllRedirects, resolveWithFullResponse, jar })
   console.log(response)
-  // let response = await axios.default.get('http://baidu.com')
-  // console.log(response)
 }
 
 main()
